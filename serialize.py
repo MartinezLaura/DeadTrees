f88859b6078dd6235927354a8fa24ff65c520b7c
madi <dileomargherita@gmail.com>
2017-01-10 13:38:19 +0100

    added description to functions save and read

--- serialize.py
+++ serialize.py
@@ -3,14 +3,22 @@ __license__ = "GPL"
 __version__ = "1.0"
 __email__ = "lmartisa@gmail.com"
 
+
+
 import pickle
 
 def save(pathpickleclip, Mylist):
-	with open(pathpickleclip+'.pickle', 'wb') as handle:
+	'''This function uses pickle to convert a python object in RAM into a
+	character stream (file)
+	'''
+	with open(pathpickleclip + '.pickle', 'wb') as handle:
 		pickle.dump(Mylist, handle)
 
 
 def read(pathpickleclip):
-	with open(pathpickleclip+'.pickle', 'rb') as handle:
+	'''This function uses pickle to load in RAM a python object stored as a
+	character stream
+	'''
+	with open(pathpickleclip + '.pickle', 'rb') as handle:
 		Mylist = pickle.load(handle)
 	return Mylist
