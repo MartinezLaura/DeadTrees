f7dc93d996bcc6a46846e36f82d786431e3aabba
madi <dileomargherita@gmail.com>
2017-02-16 16:01:54 +0100

    improved code readability

--- clipshape.py
+++ clipshape.py
@@ -13,28 +13,28 @@ import time
 
 #import matplotlib.pyplot as plt
 
-def ImageToClassify(imgClass, Bool):
-    imgarray = gdalnumeric.LoadFile(imgClass)
-    imgOriginal = np.concatenate(imgarray.T)
-    img = gdal.Open(imgClass)
-    shpOriginal = imgarray.shape
-
-    if Bool ==True:
-        imgaux = img.ReadAsArray()
-        imgaaux = imgaux.astype(float)
-        imgOriginal = gdal.GetDriverByName('GTiff').Create('newbands.tif', imgarray.shape[2], imgarray.shape[1], 5,gdal.GDT_UInt16)
-        imgOriginal.GetRasterBand(1).WriteArray((((imgaaux[3]-imgaaux[0]) / (imgaaux[3]+imgaaux[0]))+1)*127.5)
-        imgOriginal.GetRasterBand(2).WriteArray((((imgaaux[1]-imgaaux[0]) / (imgaaux[1]+imgaaux[0]))+1)*127.5)
-        imgOriginal.GetRasterBand(4).WriteArray(((imgaaux[1]-imgaaux[2]) / (imgaaux[1]+imgaaux[2])+1)*127.5)
-        imgOriginal.GetRasterBand(4).WriteArray(((imgaaux[0]-imgaaux[2]) / (imgaaux[0]+imgaaux[2])+1)*127.5)
-        imgOriginal.GetRasterBand(5).WriteArray((((imgaaux[3]-imgaaux[1]) / (imgaaux[3]+imgaaux[1])+1)*127.5))
-        imgOriginal = imgOriginal.ReadAsArray()
-        shpOriginal = imgOriginal.shape
-        imgOriginal = np.concatenate(imgOriginal.T)
-
-    projection = img.GetProjection()
-    geotrans = img.GetGeoTransform()
-    return projection, geotrans, imgOriginal, shpOriginal
+# def ImageToClassify(imgClass, Bool):
+#     imgarray = gdalnumeric.LoadFile(imgClass)
+#     imgOriginal = np.concatenate(imgarray.T)
+#     img = gdal.Open(imgClass)
+#     shpOriginal = imgarray.shape
+#
+#     if Bool ==True:
+#         imgaux = img.ReadAsArray()
+#         imgaaux = imgaux.astype(float)
+#         imgOriginal = gdal.GetDriverByName('GTiff').Create('newbands.tif', imgarray.shape[2], imgarray.shape[1], 5,gdal.GDT_UInt16)
+#         imgOriginal.GetRasterBand(1).WriteArray((((imgaaux[3]-imgaaux[0]) / (imgaaux[3]+imgaaux[0]))+1)*127.5)
+#         imgOriginal.GetRasterBand(2).WriteArray((((imgaaux[1]-imgaaux[0]) / (imgaaux[1]+imgaaux[0]))+1)*127.5)
+#         imgOriginal.GetRasterBand(3).WriteArray(((imgaaux[1]-imgaaux[2]) / (imgaaux[1]+imgaaux[2])+1)*127.5)
+#         imgOriginal.GetRasterBand(4).WriteArray(((imgaaux[0]-imgaaux[2]) / (imgaaux[0]+imgaaux[2])+1)*127.5)
+#         imgOriginal.GetRasterBand(5).WriteArray((((imgaaux[3]-imgaaux[1]) / (imgaaux[3]+imgaaux[1])+1)*127.5))
+#         imgOriginal = imgOriginal.ReadAsArray()
+#         shpOriginal = imgOriginal.shape
+#         imgOriginal = np.concatenate(imgOriginal.T)
+#
+#     projection = img.GetProjection()
+#     geotrans = img.GetGeoTransform()
+#     return projection, geotrans, imgOriginal, shpOriginal
 
 #Uses a gdal geomatrix (gdal.GetGeoTransform()) to calculate the pixel location of a geospatial coordinate
 def world2Pixel(geoMatrix, x, y):
@@ -57,7 +57,7 @@ def imageToArray(i):
     a.shape = i.im.size[1], i.im.size[0]
     return a
 
-def NewBands(clipFeat, lrY, ulY,lrX, ulX):
+def NewBands(clipFeat, lrY, ulY, lrX, ulX):
     # #new indx create new layer
     # clip = np.empty((4,lrY- ulY,lrX- ulX))
     # clip[0] = clipFeat[0].astype(np.uint8)
@@ -68,28 +68,28 @@ def NewBands(clipFeat, lrY, ulY,lrX, ulX):
     # clip[3] = nirg.astype(np.uint8)
 
     #regular ind
-    clip = np.empty((5,lrY- ulY,lrX- ulX))
-    ndvi = (((clipFeat[3]-clipFeat[0]) / (clipFeat[3]+clipFeat[0]))+1)*127.5
+    clip = np.empty((5, lrY - ulY, lrX - ulX))
+    ndvi = (((clipFeat[3] - clipFeat[0]) / (clipFeat[3] + clipFeat[0])) + 1) * 127.5
     clip[0] = ndvi.astype(np.uint8)
-    gr = (((clipFeat[1]-clipFeat[0]) / (clipFeat[1]+clipFeat[0]))+1)*127.5
+    gr   = (((clipFeat[1] - clipFeat[0]) / (clipFeat[1] + clipFeat[0])) + 1) * 127.5
     clip[1] = gr.astype(np.uint8)
-    bg = ((clipFeat[1]-clipFeat[2]) / (clipFeat[1]+clipFeat[2])+1)*127.5
+    bg   =  ((clipFeat[1] - clipFeat[2]) / (clipFeat[1] + clipFeat[2])  + 1) * 127.5
     clip[2] = bg.astype(np.uint8)
-    br = ((clipFeat[0]-clipFeat[2]) / (clipFeat[0]+clipFeat[2])+1)*127.5
+    br   =  ((clipFeat[0] - clipFeat[2]) / (clipFeat[0] + clipFeat[2])  + 1) * 127.5
     clip[3] = br.astype(np.uint8)
-    nirg = ((clipFeat[3]-clipFeat[1]) / (clipFeat[3]+clipFeat[1])+1)*127.5
+    nirg =  ((clipFeat[3] - clipFeat[1]) / (clipFeat[3] + clipFeat[1])  + 1) * 127.5
     clip[4] = nirg.astype(np.uint8)
 
     return clip
 
-def ReadClipArray(lrY, ulY,lrX, ulX,img):
-    clip = np.empty((img.RasterCount,lrY- ulY,lrX- ulX))
+def ReadClipArray(lrY, ulY, lrX, ulX, img):
+    clip = np.empty((img.RasterCount, lrY - ulY, lrX - ulX))
 
     #Read only the pixels needed for do the clip
     for band in range(img.RasterCount):
         band += 1
-        imgaux = img.GetRasterBand(band).ReadAsArray(ulX,ulY,lrX- ulX,lrY- ulY)
-        clip[band-1] = imgaux
+        imgaux = img.GetRasterBand(band).ReadAsArray(ulX, ulY, lrX - ulX, lrY - ulY)
+        clip[band - 1] = imgaux
     return clip
 
 
@@ -99,7 +99,7 @@ def ObtainPixelsfromShape(field, rasterPath, shapePath, INX):
     # field='zona'
     # open dataset, also load as a gdal image to get geotransform
     # INX can be false. If True, uses additional layers.
-    print"Starting clip...."
+    print "Starting clip...."
     start = time.time()
 
     img = gdal.Open(rasterPath)
@@ -126,17 +126,17 @@ def ObtainPixelsfromShape(field, rasterPath, shapePath, INX):
         #print ulX,lrX,ulY,lrY
 
         # Calculate the pixel size of the new image
-        pxWidth = int(lrX - ulX)
+        pxWidth  = int(lrX - ulX)
         pxHeight = int(lrY - ulY)
 
         if INX == True:
-            clip = ReadClipArray(lrY, ulY,lrX, ulX,img)
-            clip = NewBands(clip,lrY, ulY,lrX, ulX)
+            clip = ReadClipArray(lrY, ulY, lrX, ulX, img)
+            clip = NewBands(clip, lrY, ulY, lrX, ulX)
 
 
         else:
             #print"Indexes = False"
-            clip = ReadClipArray(lrY, ulY,lrX, ulX,img)
+            clip = ReadClipArray(lrY, ulY, lrX, ulX, img)
 
 
         # print clip[0]
@@ -184,19 +184,19 @@ def ObtainPixelsfromShape(field, rasterPath, shapePath, INX):
         # plt.show()
 
         # Clip the image using the mask into a dict
-        temp=gdalnumeric.choose(mask,(clip, np.nan))
+        temp = gdalnumeric.choose(mask, (clip, np.nan))
 
         # #SHow the clips of the image
         # plt.imshow(temp[4])
         # plt.show()
         temp = np.concatenate(temp.T)
-        temp = temp[~np.isnan(temp[:,0])]
+        temp = temp[~np.isnan(temp[:, 0])]
 
 
         clipdic[str(feature.GetField(field))].append(temp)
         count += temp.shape[0]
     end = time.time()
     print "Time clipshape:"
-    print (end-start)
-    return clipdic,count
+    print (end - start)
+    return clipdic, count
 ##########################################################################
