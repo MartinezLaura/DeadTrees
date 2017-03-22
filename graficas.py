e395a3f7d1caead9025b32f086b5d61616c9d50e
madi <dileomargherita@gmail.com>
2017-03-22 15:45:19 +0100

    improved readability

--- graficas.py
+++ graficas.py
@@ -12,8 +12,8 @@ from sklearn import cross_validation
 from sklearn.learning_curve import learning_curve
 
 
-def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None,
-                        n_jobs=1, train_sizes=np.linspace(.1, 1.0, 5)):
+def plot_learning_curve(estimator, title, X, y, ylim = None, cv = None,
+                        n_jobs = 1, train_sizes = np.linspace(.1, 1.0, 5)):
     #this is plot at page 11 of presentation 2.
     """
     Generate a simple plot of the test and traning learning curve.
@@ -51,23 +51,37 @@ def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None,
         plt.ylim(*ylim)
     plt.xlabel("Training examples")
     plt.ylabel("Score")
-    train_sizes, train_scores, test_scores = learning_curve(
-        estimator, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes)
-    train_scores_mean = np.mean(train_scores, axis=1)
-    train_scores_std = np.std(train_scores, axis=1)
-    test_scores_mean = np.mean(test_scores, axis=1)
-    test_scores_std = np.std(test_scores, axis=1)
+    train_sizes, train_scores, test_scores = learning_curve(estimator, \
+                        X, y, cv = cv, \
+                        n_jobs = n_jobs, \
+                        train_sizes = train_sizes)
+
+    train_scores_mean = np.mean(train_scores, axis = 1)
+    train_scores_std = np.std(train_scores, axis = 1)
+    test_scores_mean = np.mean(test_scores, axis = 1)
+    test_scores_std = np.std(test_scores, axis = 1)
     plt.grid()
 
-    plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
-                     train_scores_mean + train_scores_std, alpha=0.1,
-                     color="r")
-    plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
-                     test_scores_mean + test_scores_std, alpha=0.1, color="g")
-    plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
-             label="Training score")
-    plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
-             label="Cross-validation score")
+    plt.fill_between(train_sizes, \
+                        train_scores_mean - train_scores_std, \
+                        train_scores_mean + train_scores_std, \
+                        alpha = 0.1, \
+                        color = "r")
 
-    plt.legend(loc="best")
+    plt.fill_between(train_sizes, \
+                        test_scores_mean - test_scores_std, \
+                        test_scores_mean + test_scores_std, \
+                        alpha = 0.1, \
+                        color = "g")
+
+    plt.plot(train_sizes, train_scores_mean, \
+                        'o-', \
+                        color = "r", \
+                        label = "Training score")
+
+    plt.plot(train_sizes, test_scores_mean, \
+                        'o-', color="g", \
+                        label = "Cross-validation score")
+
+    plt.legend(loc = "lower right")
     return plt
